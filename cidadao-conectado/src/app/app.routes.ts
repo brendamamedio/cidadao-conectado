24b1c4b30e202c0385fd9e0fa33cb8683fe51803
Brenda <132839420+brendamamedio@users.noreply.github.com>
2025-02-19 19:58:07 +0000

    ajustes tela inicial e categorias

--- cidadao-conectado/src/app/app.routes.ts
+++ cidadao-conectado/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { CheckListComponent } from './pages/check-list/check-list.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ModuloComponent } from './pages/modulo/modulo.component';
 
 export const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
       { path: 'check-list', component: CheckListComponent },
-      { path: '', redirectTo: '/check-list', pathMatch: 'full' }
+      { path: 'home', component: HomeComponent },
+      { path: 'modulo', component: ModuloComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' }
     ]
   },
   { path: 'signup', component: SignupComponent },
